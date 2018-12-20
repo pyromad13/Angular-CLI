89a9e1c4cf908d87b41ba37b10d48da32d3f9d5b
nauagio13 <pyromalis@gmail.com>
2018-12-20 13:49:31 +0000

    Create sales service and search box directive

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
+import { SearchBoxDirective } from './search-box.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomerComponent,
+    SearchBoxDirective
   ],
   imports: [
     BrowserModule,
